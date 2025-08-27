967edd8a1f429e305f5c0835ae464572ca2a9627
Divenchy <lfexp35@gmail.com>
2025-08-26 22:02:09 -0500

    added tamuvpn install script

--- modules/tamuVPN/vpn.nix
+++ modules/tamuVPN/vpn.nix
@@ -2,7 +2,7 @@
 
 {
   system.activationScripts.installTAMUVPN = lib.fileContents ''
-  chmod +x ./cisco-secure-client-linux64-5.1.10.233-core-vpn-webdeploy-k9.sh
-  sudo ./cisco-secure-client-linux64-5.1.10.233-core-vpn-webdeploy-k9.sh
+  chmod +x /home/weo/nixweo/modules/tamuVPN/cisco-secure-client-linux64-5.1.10.233-core-vpn-webdeploy-k9.sh
+  sudo /home/weo/nixweo/modules/tamuVPN/cisco-secure-client-linux64-5.1.10.233-core-vpn-webdeploy-k9.sh
   '';
 }
