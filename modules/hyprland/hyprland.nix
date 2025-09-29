4cf54cd737d82bcbdedeed1ee9bda8f067060f2a
Divenchy <lfexp35@gmail.com>
2025-09-28 23:17:55 -0500

    fixed - on brightnessup

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -140,7 +140,7 @@
           "$mod CTRL, F5, exec, hyprshot -m output"
 
           ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
-          ", XF86MonBrightnessUp, exec, brightnessctl set 5%-"
+          ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
           ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_SINK@ 5%-"
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
