fdca02522802e8ab613fb8eec00b56ea14e2b974
Divenchy <lfexp35@gmail.com>
2026-01-16 00:18:13 -0600

    added more pure lua for more complex lua

--- modules/weovim/nvim.nix
+++ modules/weovim/nvim.nix
@@ -1,29 +1,63 @@
-{pkgs, ...}:
+{ pkgs, ... }:
+let
+  optionsLua = ./lua/options.lua;
+  remapsLua = ./lua/remaps.lua;
+  auAutocmdsLua = ./lua/auAutocmdsLua.lua;
+in
 {
   viAlias = true;
   vimAlias = true;
 
   # Set leader key
   globals = {
-    mapleader = " ";      # Space as leader
+    mapleader = " "; # Space as leader
     maplocalleader = ","; # Comma as local leader (optional)
   };
 
   ###### OPTIONS  #######
   options = {
+    guicursor = "";
+    mouse = "a";
+    
     number = true;
     relativenumber = true;
+
+    tabstop = 2;
+    softtabstop = 2;
+    shiftwidth = 2;
     expandtab = true;
-    shiftwidth = 4;
-    tabstop = 4;
+    cursorline = true;
+
+    smartindent = true;
+    breakindent = true;
+    showmode = false;
+
+    ignorecase = true;
+    smartcase = true;
+
+    wrap = false;
+
+    swapfile = false;
+    backup = false;
+
+    termguicolors = true;
+
+    scrolloff = 20;
+    signcpolumn = "yes";
   };
+
+  extraConfigLua = ''
+  dofile("${optionsLua}")
+  dofile("${remapsLua}")
+  dofile("${auAutocmdsLua}")
+  '';
   
+
   statusline.lualine.enable = true;
   telescope.enable = true;
   treesitter.enable = true;
   autocomplete.nvim-cmp.enable = true;
   git.enable = true;
-
 
   ###### LSP  #######
 
@@ -31,12 +65,13 @@
     enable = true;
     formatOnSave = true;
   };
-  
+
   languages = {
     enableTreesitter = true;
     nix.enable = true;
     rust.enable = true;
     zig.enable = true;
     clang.enable = true;
+    csharp.enable = true;
   };
 }
