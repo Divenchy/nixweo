a0c51507cf13ec768bcec6be841258e48989e4b5
Divenchy <lfexp35@gmail.com>
2026-02-17 00:16:25 -0600

    added man pages

--- hosts/nixweo/configuration.nix
+++ hosts/nixweo/configuration.nix
@@ -262,6 +262,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    man-pages
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git-lfs
     cmake
